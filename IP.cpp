52e99027ca360ad7aaa22e714ce4a203f1f9e5ac
Richard Kocián <richardkocian4@gmail.com>
2023-11-20 10:17:22 +0100

    removed additional couts

--- IP.cpp
+++ IP.cpp
@@ -13,11 +13,8 @@ IP::IP(string ip) {
     ip_addr_str = ip.substr(0, pos);
     ip_mask_str = ip.substr(pos + 1, ip.length());
 
-    //cout << "IP is: " << ip_addr << endl;
-    //cout << "Prefix is: " << ip_mask << endl;
-
     mask = stoi(ip_mask_str);
-    //cout << "Prefix is: " << mask << endl;
+
     createIP();
     createIPPrefix();
 }
@@ -32,14 +29,8 @@ void IP::createIP() {
     copy.erase(0, copy.find('.')+1);
     string octet4 = copy.substr(0, copy.find('.'));
     copy.erase(0, copy.find('.')+1);
-/*    cout << "octet1 is: " << octet1 << endl;
-    cout << "octet2 is: " << octet2 << endl;
-    cout << "octet3 is: " << octet3 << endl;
-    cout << "octet4 is: " << octet4 << endl;*/
 
     ip = (stoi(octet1) << 24) + (stoi(octet2) << 16) + (stoi(octet3) << 8) + stoi(octet4);
-
-    //cout << "IP is: " << ip << endl;
 
     allocated_addresses = 0;
 }
@@ -51,27 +42,14 @@ int IP::getIP() {
 void IP::createIPPrefix() {
 
     unsigned int all_ones = 4294967295;
-    //cout << "Mask is: " << mask << endl;
+
     int ones_at_end = (1 << (32-mask)) -1;
-    // cout << "xoruju: " << endl;
-    // std::cout << "Maska binární: " << std::bitset<32>(ones_at_end) << std::endl;
-    // std::cout << "All ones: " << std::bitset<32>(all_ones) << std::endl;
 
     mask_binary = ones_at_end ^ all_ones;
-    //std::cout << std::bitset<32>(mask_binary) << " - Maska" << std::endl;
-
-    //std::cout << "IP binární: " << std::bitset<32>(ip) << std::endl;
-    //std::cout << "IP string: " << ip_addr_str << std::endl;
 
     ip_network_addr = ip & mask_binary;
 
-    //cout << "IP_prefix is: " << ip_prefix << endl;
-
-    //std::cout << std::bitset<32>(ip_network_addr) << " IP prefix binární: " << std::endl;
-
     ip_broadcast_addr = ip_network_addr | ones_at_end;
-
-    //std::cout << std::bitset<32>(ip_broadcast_addr) <<  " IP last addre: " << std::endl;
 
     max_addresses = pow(2, (32-mask)) - 2;
 }
@@ -79,7 +57,6 @@ void IP::createIPPrefix() {
 void IP::is_IP_in_range(int ip) {
     if (ip < ip_broadcast_addr && ip > ip_network_addr) {
         allocated_addresses++;
-        //cout << "V range " << ip_addr_str << "/" << ip_mask_str << " je IP: " << ip << endl;
     }
 }
 
